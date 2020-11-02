0883e121976a1b159b663eaab7732dad01fe3dce
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-11-02 17:15:31 +0100

    docs(item): add more docs for item

--- database/models/item.js
+++ database/models/item.js
@@ -5,17 +5,30 @@
  *   schemas:
  *     Item:
  *       type: object
+ *       required: [name, status, notes, listId]
  *       properties:
  *         id:
  *           type: integer
+ *           readOnly: true
+ *           format: ObjectId
+ *           description: |
+ *             Unique identifier (`ObjectId`).
+ *             Generated automatically during creation.
+ *           example: '507f1f77bcf86cd799439011'
  *         name:
  *           type: string
+ *           description: A name to item
+ *           example: Compliment
  *         status:
  *           type: string
+ *           enum: ['active', 'completed']
  *         notes:
  *           type: string
+ *           description: A note to item
+ *           example: You're amazing
  *         listId:
  *           type: integer
+ *           example: 1
  *     Items:
  *         type: array
  *         items:
--- server/modules/item/router.js
+++ server/modules/item/router.js
@@ -10,7 +10,8 @@ const routes = new Router();
  *    get:
  *      tags:
  *          - Item
- *      summary: this should list all item.
+ *      summary: this should list all of items.
+ *      description: List all of items.
  *      consumes:
  *        - application/json
  *      responses:
@@ -26,8 +27,81 @@ const routes = new Router();
  *          $ref: '#/components/schemas/Items'
  */
 routes.get('/item', controllers.getMany);
+
+  /**
+   * @swagger
+   * /item:
+   *   post:
+   *     tags: 
+   *        - Item
+   *     summary: this should create an item.
+   *     description: Create new `Item`.
+   *     produces:
+   *       - 
+   *       - application/json
+   *       - text/plain
+   *     parameters:
+   *       - in: body
+   *         name: body
+   *         required: true
+   *         schema:
+   *           $ref: '#/components/schemas/Item'
+   *         description: The `Item` entity to create.
+   *     responses:
+   *       200:
+   *         description: The `Item` was created successfully.
+   *         schema:
+   *           type: object
+   *           properties:
+   *             status:
+   *               type: string
+   *               example: success
+   *             data:
+   *               $ref: '#/components/schemas/Item'
+   *             errors:
+   *               description: An empty array (always when success).
+   *               type: array
+   *               items:
+   *                 type: object
+   *               example: []
+   *       400:
+   *         description: Invalid input data.
+   *       404:
+   *         description: Non-existent `ITEM` or lack of permission to see it.
+   *
+   */
 routes.post('/item', controllers.createOne);
 
+
+/**
+ * @swagger
+ * /item/{id}:
+ *    get:
+ *      tags:
+ *          - Item
+ *      summary: this should list an item by id.
+ *      description: List an item by id.
+ *      parameters:
+ *        - in: path
+ *          name: id
+ *          required: true
+ *          type: string
+ *          format: ObjectId
+ *          description: ID of Item to be retrieved
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Receive back flavor and flavor Id of recently added cookie.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Items'
+ *      schema:
+ *          $ref: '#/components/schemas/Items'
+ */
 routes.get('/item/:id', controllers.getOne);
 routes.delete('/item/:id', controllers.removeOne);
 routes.put('/item/:id', controllers.updateOne);
