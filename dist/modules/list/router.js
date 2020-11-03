1350fc293f0d58ecad871761f1e16dbbc4bd68e6
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-11-03 10:04:13 +0100

    docs(list): add doc for get all lists

--- dist/modules/list/router.js
+++ dist/modules/list/router.js
@@ -12,6 +12,29 @@ var _express = require("express");
 var _controllers = _interopRequireDefault(require("./controllers"));
 
 var routes = new _express.Router();
+/**
+ * @swagger
+ * /list:
+ *    get:
+ *      tags:
+ *          - List
+ *      summary: this should list all of lists.
+ *      description: List all of `Lists`.
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Get a list of Lists.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Lists'
+ *      schema:
+ *          $ref: '#/components/schemas/Lists'
+ */
+
 routes.get('/list', _controllers["default"].getMany);
 routes.post('/list', _controllers["default"].createOne);
 routes.get('/list/:id', _controllers["default"].getOne);
