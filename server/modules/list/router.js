e87b763abaac90b225491fff366e6ed504793839
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-11-03 10:04:13 +0100

    docs(list): add doc for get all lists

--- server/modules/list/router.js
+++ server/modules/list/router.js
@@ -3,6 +3,28 @@ import controllers from './controllers';
 
 const routes = new Router();
 
+/**
+ * @swagger
+ * /list:
+ *    get:
+ *      tags:
+ *          - List
+ *      summary: this should list all of lists.
+ *      description: List all of `Lists`.
+ *      consumes:
+ *        - application/json
+ *      responses:
+ *        200:
+ *          description: Get a list of Lists.
+ *          content:
+ *            application/json:
+ *              schema:
+ *                type: array
+ *                items:
+ *                  $ref: '#/components/schemas/Lists'
+ *      schema:
+ *          $ref: '#/components/schemas/Lists'
+ */
 routes.get('/list', controllers.getMany);
 routes.post('/list', controllers.createOne);
 
