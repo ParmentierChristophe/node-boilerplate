6add08b5048778d8cc238cfc75575be62da2fde5
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-11-03 09:56:41 +0100

    docs(list): add doc list model

--- database/models/list.js
+++ database/models/list.js
@@ -1,4 +1,34 @@
 'use strict';
+
+/**
+ * @openapi
+ * components:
+ *   schemas:
+ *     List:
+ *       type: object
+ *       required: [name, description]
+ *       properties:
+ *         id:
+ *           type: integer
+ *           readOnly: true
+ *           format: ObjectId
+ *           description: |
+ *             Unique identifier (`ObjectId`).
+ *             Generated automatically during creation.
+ *           example: '507f1f77bcf86cd799439011'
+ *         name:
+ *           type: string
+ *           description: A name to list
+ *           example: Achievement
+ *         description:
+ *           type: string
+ *           description: A Description to list
+ *           example: His biggest achievement so far
+ *     Lists:
+ *         type: array
+ *         items:
+ *            $ref: '#/components/schemas/List'
+ */
 module.exports = (sequelize, DataTypes) => {
   const List = sequelize.define(
     'List',
