0ab3b54a0fd7c5e147b20221649f1bca93e322eb
Christophe Parmentier <christophe.parmentier@ineat-conseil.fr>
2020-12-23 11:48:06 +0100

    add authorization to swagger documentation

--- dist/modules/list/router.js
+++ dist/modules/list/router.js
@@ -9,6 +9,8 @@ exports["default"] = void 0;
 
 var _express = require("express");
 
+var _auth = require("../../config/passport/utils/auth");
+
 var _controllers = _interopRequireDefault(require("./controllers"));
 
 var routes = new _express.Router();
@@ -22,6 +24,8 @@ var routes = new _express.Router();
  *      description: List all of `Lists`.
  *      consumes:
  *        - application/json
+ *      security:
+ *        - ApiKeyAuth: []
  *      responses:
  *        200:
  *          description: Get a list of Lists.
@@ -35,8 +39,55 @@ var routes = new _express.Router();
  *          $ref: '#/components/schemas/Lists'
  */
 
-routes.get('/list', _controllers["default"].getMany);
-routes.post('/list', _controllers["default"].createOne);
+routes.get('/list', _auth.auth.required, _controllers["default"].getMany);
+/**
+ * @swagger
+ * /list:
+ *   post:
+ *     tags:
+ *        - List
+ *     summary: this should create a list.
+ *     description: Create new `List`.
+ *     produces:
+ *       - application/json
+ *     consumes:
+ *       - application/json
+ *     security:
+ *       - ApiKeyAuth: []
+ *     requestBody:
+ *         required: true
+ *         content:
+ *           application/x-www-form-urlencoded:
+ *             schema:
+ *                $ref: '#/components/schemas/List'
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/List'
+ *         description: The `List` entity to create.
+ *     responses:
+ *       200:
+ *         description: The `List` was created successfully.
+ *         schema:
+ *           type: object
+ *           properties:
+ *             status:
+ *               type: string
+ *               example: success
+ *             data:
+ *               $ref: '#/components/schemas/List'
+ *             errors:
+ *               description: An empty array (always when success).
+ *               type: array
+ *               items:
+ *                 type: object
+ *               example: []
+ *       400:
+ *         description: Invalid input data.
+ *       404:
+ *         description: Non-existent `ITEM` or lack of permission to see it.
+ */
+
+routes.post('/list', _auth.auth.required, _controllers["default"].createOne);
 routes.get('/list/:id', _controllers["default"].getOne);
 routes["delete"]('/list/:id', _controllers["default"].removeOne);
 routes.put('/list/:id', _controllers["default"].updateOne);
